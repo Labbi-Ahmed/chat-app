52a01f87fd3513ec42ff24607b67f3892b4964f8
Labbi-Ahmed <labbi5689@gmail.com>
2024-09-22 23:52:07 +0600

    first commit

---
+++ src/main/java/com/labbi/chat_app/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.labbi.chat_app.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/labbi/chat_app/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.labbi.chat_app.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
